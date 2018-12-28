513d4d02f52dad799740ddb7734310f8e296d09d
Vinayak Subhash Kunder <vinayakkunder97@gmail.com>
2018-12-28 20:19:32 +0530

    Added CNN Layers

--- train_model.py
+++ train_model.py
@@ -5,4 +5,17 @@ from keras.layers import Flatten
 from keras.layers import Dense, Dropout
 from keras import optimizers
 
+#initialize cnn
 classifier = Sequential()
+
+#CNN Layer- 1
+classifier.add(Convolution2D(32, 3,  3, input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size =(2,2)))
+
+#CNN Layer- 2
+classifier.add(Convolution2D(32, 3,  3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size =(2,2)))
+
+#CNN Layer- 3
+classifier.add(Convolution2D(64, 3,  3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size =(2,2)))
