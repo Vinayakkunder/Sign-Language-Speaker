936a4cf4871164a691394f39380ff018479909f7
Vinayak Subhash Kunder <vinayakkunder97@gmail.com>
2018-12-28 21:17:16 +0530

    fitting the CNN

--- train_model.py
+++ train_model.py
@@ -32,3 +32,33 @@ classifier.add(Dense(5, activation = 'softmax'))
 classifier.compile(optimizer = optimizers.SGD(lr = 0.01),
               loss = 'categorical_crossentropy',
               metrics = ['accuracy'])
+
+#Fitting CNN to image
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+        'mydata/training_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='categorical')
+
+test_set = test_datagen.flow_from_directory(
+        'mydata/test_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='categorical')
+
+model = classifier.fit_generator(
+        training_set,
+        steps_per_epoch=200,
+        epochs=8,
+        validation_data = test_set,
+        validation_steps = 6500
+      )
