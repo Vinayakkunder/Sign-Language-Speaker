07c4b2f410e6704a47af8c5f7f3df8168034b618
Vinayak Subhash Kunder <vinayakkunder97@gmail.com>
2018-12-28 20:10:59 +0530

    Initialization
    Initialized CNN Layer and imported required libraries

---
+++ train_model.py
@@ -0,0 +1,8 @@
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense, Dropout
+from keras import optimizers
+
+classifier = Sequential()
